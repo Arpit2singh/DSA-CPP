1488b60ec58208e6b89c83869fdee60009a6cfb2
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-08-18 00:40:44 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.93%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int check(int n , vector<int>&dp){
+        if(n == 0) return  0 ; 
+        if(n == 1) return 1 ;  
+        if(n == 2) return 2 ; 
+        if(dp[n] != -1) return dp[n] ; 
+
+        return dp[n] = check(n-1 , dp) + check(n-2 , dp) ; 
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1 , -1) ; 
+        return check(n , dp) ; 
+    }
+};
