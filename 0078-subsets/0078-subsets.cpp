28eba7faf1c0cf8512824b06e8c281c54c432c9a
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-08-18 19:03:43 +0530

    Time: 0 ms (100%), Space: 10.3 MB (27.93%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    void check(vector<int>&nums , int idx , vector<vector<int>>&v , vector<int>temp) {
+    void check(vector<int>&nums , int idx , vector<int>temp ,  vector<vector<int>>&v){
         v.push_back(temp) ; 
 
         for(int i = idx ; i<nums.size() ; i++){
             temp.push_back(nums[i]) ; 
-            check(nums , i+1 , v , temp) ; 
+            check(nums , i+1 , temp , v) ; 
             temp.pop_back() ; 
         }
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>>v ; 
         vector<int>temp ; 
-        int idx = 0 ; 
-        check(nums , idx , v , temp) ; 
+        check(nums , 0 , temp , v ) ; 
+
         return v ; 
+
     }
 };
