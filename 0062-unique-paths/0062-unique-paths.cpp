4883781ab759cbbb11e9b5315e4860967516c3ae
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-08-19 11:10:18 +0530

    Time: 0 ms (100%), Space: 9.2 MB (71.49%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int check(int m , int n , vector<vector<int>>&dp){
+       
+        if(m==0 && n==0 ) return 0 ; 
+        if(m==0 || n== 0) return 0 ;  
+        if(dp[m][n] != -1) return dp[m][n] ; 
+
+        return dp[m][n] = 1 + check(m-1 , n , dp) + check(m , n-1 , dp) ;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1 , vector<int>(n+1 , -1)) ; 
+        return 1+check(m-1 , n-1 , dp) ; 
+    }
+};
