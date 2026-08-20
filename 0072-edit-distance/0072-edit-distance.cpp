38df2397b6637e14e288abacdd0d1ad13e1b888f
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-08-20 12:10:23 +0530

    Time: 2 ms (93.84%), Space: 14.3 MB (17.89%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int check(string &word1 , string&word2 , vector<vector<int>>&dp , int i , int j){
-        if(i>=word1.size()) return word2.size()-j ;  
-        if(j>=word2.size()) return word1.size()-i ; 
+    int check(string&word1 , string &word2 , int i , int j ,vector<vector<int>>&dp){
+        if(i >= word1.size()) return word2.size()-j ; 
+        if(j >= word2.size()) return word1.size()-i ;   
 
         if(dp[i][j] != -1) return dp[i][j] ; 
         if(word1[i] == word2[j]){
-            return dp[i][j]=check(word1 , word2 , dp , i+1 , j+1) ; 
+            return dp[i][j] = check(word1 , word2 , i+1 , j+1 , dp) ; 
         }
-        return dp[i][j] = 1 + min({check(word1 , word2 , dp , i+1 , j) , check(word1 , word2 , dp, i , j+1) , check(word1 , word2 , dp , i+1 , j+1)}) ;  
+        return dp[i][j] = 1 + min({check(word1 , word2 , i+1 , j , dp) , check(word1 , word2 , i , j+1 , dp) , check(word1 , word2 , i+1 , j+1 , dp)  }) ; 
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size() , vector<int>(word2.size() , -1)) ; 
-        return check(word1 , word2 , dp , 0 , 0) ; 
+        vector<vector<int>>dp(word1.size()+1 , vector<int>(word2.size()+1 , -1)) ; 
+        int i = 0 ; 
+        int j = 0 ; 
+
+        return check(word1 , word2 , i , j , dp) ; 
     }
 };
