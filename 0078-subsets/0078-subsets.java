c3e9ebc1b0ff4f64719c933892fdc7a61bf28267
ARPIT SINGH <168482122+Arpit2singh@users.noreply.github.com>
2026-09-18 10:58:49 +0530

    Time: 1 ms (87.16%), Space: 44.3 MB (38.8%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    void check(int[] nums , int idx , List<Integer>temp , List<List<Integer>>v){
+        v.add(new ArrayList<>(temp)); 
+
+        for(int i = idx ; i<nums.length ; i++){
+            temp.add(nums[i]); 
+            check(nums , i + 1 , temp , v) ; 
+            temp.remove(temp.size()-1) ; 
+        }
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>ans = new ArrayList<>() ; 
+        List<Integer>temp = new ArrayList<>() ; 
+
+        check(nums , 0 , temp , ans) ; 
+
+        return ans ; 
+
+    }
+}
